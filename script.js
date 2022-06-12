a6c0e7e2488f0bf38f24c803edf249a4b6d4a627
RuanVPSantos <ruanvpsantos@gmail.com>
2022-06-12 15:06:30 -0300

    Finalização
    Finalizando cópia do cliente da Riot Games.

---
+++ script.js
@@ -0,0 +1,30 @@
+const inputs = document.querySelectorAll(".input")
+const button = document.querySelector('.login__button')
+
+const handleFocus = ({target}) => {
+  const span = target.previousElementSibling;
+  span.classList.add("span-active");
+}
+
+const handleOutFocus = ({target}) => {
+  const span = target.previousElementSibling;
+  if (target.value === "") {
+    span.classList.remove("span-active");
+  }
+}
+
+const handleChange = () =>
+{
+  const [username, password] = inputs;
+
+  if (username.value && password.value.length >= 8) {
+    button.removeAttribute("disabled");
+  } else {
+    button.setAttribute("disabled", "");
+  }
+}
+
+
+inputs.forEach((input) => input.addEventListener("focus", handleFocus));
+inputs.forEach((input) => input.addEventListener("focusout", handleOutFocus));
+inputs.forEach((input) => input.addEventListener("input", handleChange));
